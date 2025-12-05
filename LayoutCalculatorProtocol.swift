395b68bf83692df1575bc6140ea28f167f6a8c1c
bjyitu <bjyitu@gmail.com>
2025-12-05 23:08:23 +0800

    Delete LayoutCalculatorProtocol.swift

--- LayoutCalculatorProtocol.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-/// 布局计算器协议 - 统一两种布局计算器的接口
-protocol LayoutCalculatorProtocol {
-    func getFixedGridRows(for group: DirectoryGroup, availableWidth: CGFloat, hasReceivedGeometry: Bool) -> [FixedGridRow]
-    func calculateEffectiveWidth(availableWidth: CGFloat) -> CGFloat
-    func clearCache()
-}
